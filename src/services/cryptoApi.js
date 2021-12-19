29217a38e8601b3e4ced9ec04a145d9d585f7854
Vinicius Franca <vfranca14@gmail.com>
2021-12-19 09:57:08 -0300

    Creating cryptoApi service

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import create from '@ant-design/icons/lib/components/IconFont';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': '<YOUR_HOST>',
+  'x-rapidapi-key': '<YOUR_KEY>'
+};
+
+const baseUrl = '<YOUR_URL>';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
