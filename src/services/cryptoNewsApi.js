31d68b7165e7d2f9b2face26438f04544ed30f43
Vinicius Franca <vfranca14@gmail.com>
2021-12-21 17:23:29 -0300

    Creating crypto news API

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-host': '<YOUR_HOST>',
+  'x-rapidapi-key': '<YOUR_KEY>'
+};
+
+const baseUrl = '<YOUR_URL>';
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    })
+  })
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
