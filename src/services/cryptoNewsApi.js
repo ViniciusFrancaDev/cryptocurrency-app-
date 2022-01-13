b7af257bc7cbaa02cf74b9ba827fa744062b4a2d
Vinicius Franca <vfranca14@gmail.com>
2022-01-13 15:40:53 -0300

    Changing cryptoNewsApi service to use env vars

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
   'x-bingapis-sdk': 'true',
-  'x-rapidapi-host': '<YOUR_HOST>',
-  'x-rapidapi-key': '<YOUR_KEY>'
+  'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
 };
 
-const baseUrl = '<YOUR_URL>';
+const baseUrl = process.env.REACT_APP_NEWS_API_URL;
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
 export const cryptoNewsApi = createApi({
